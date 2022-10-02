3123542063c3d2d8adf953f454a337e1c786126c
Kevin Garcia <abelgarcia612@gmail.com>
2022-10-01 20:25:13 -0500

    Added a ui to play the game

--- RockPaperScissors.js
+++ RockPaperScissors.js
@@ -1,18 +1,29 @@
-playerScore = 0;
-computerScore = 0;
+let playerScore = 0;
+let computerScore = 0;
+
+const scores = document.querySelector('#scores');
+const results = document.querySelector('.results');
+
+const playAgainBtn = document.createElement('button');
+playAgainBtn.textContent = "Play Again!";
+playAgainBtn.addEventListener('click', playAgain );
+
+const selectionBtns = document.querySelectorAll('.selectionBtn');
+selectionBtns.forEach((btn) => {
+  btn.addEventListener('click', () => game(btn))
+}); 
 
 
 function getComputerChoice() {
     randomNum = Math.floor(Math.random()*11);
     if (randomNum < 3) {
-        return "Rock";
+        return "rock";
     }
     else if( randomNum  < 7){
-        return "Paper";
+        return "paper";
     }
-    return "Scissors";
+    return "scissors";
 }
-
 
 function playRound(playerSelection, computerSelection) {
     player = playerSelection.toLowerCase();
@@ -54,17 +65,25 @@ function playRound(playerSelection, computerSelection) {
         }
     }
 
-  }
+}
 
-  function game(){
+function game(btn){
+    if (computerScore == 5 || playerScore == 5) {
+        let message  = (playerScore > computerScore) ? `You win this round! ${playerScore} to ${computerScore}` :
+                    `You lose this round! ${playerScore} to ${computerScore}`;
+        if (scores.textContent != message) {
+            scores.textContent = message;
+            results.appendChild(playAgainBtn);
+        }
+    }
+    else {
+        scores.textContent = (playRound(btn.id, getComputerChoice()));
+    }
+}
+
+function playAgain(){
     playerScore = 0;
     computerScore = 0;
-
-    for (let i = 0; i < 5; i++){
-        playerSelection = prompt("1,2,3, Choose!")
-        console.log(playRound(playerSelection, getComputerChoice()));
-    }
-    let message  = (playerScore > computerScore) ? `You win! ${playerScore} to ${computerScore}` :
-                `You lose! ${playerScore} to ${computerScore}`;
-    console.log(message);
-  }
+    scores.textContent = "Click any of the options above to start this round";
+    results.removeChild(playAgainBtn);
+}
